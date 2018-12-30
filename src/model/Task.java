da8289d9d64df2ab05cd56a406b76a8b86d4804e
=Omid-PC <=nassiromid@gmail.com>
2018-12-30 12:17:28 -0800

    Task: Changed SimpleProperties to Properties Changed visibility for priorities Added priority and date to params to constructor Added getters/setters for said params

--- src/model/Task.java
+++ src/model/Task.java
@@ -1,25 +1,38 @@
 package model;
 
-import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.*;
+
+import java.time.LocalDate;
 
 public class Task
 {
-    private final SimpleStringProperty title;
-    private final SimpleStringProperty category;
 
-    public Task(String title, String category)
+    //PRIORITIES
+    public final static int NO_PRIORITY = 0;
+    public final static int LOW_PRIORITY = 1;
+    public final static int MEDIUM_PRIORITY = 2;
+    public final static int HIGH_PRIORITY = 3;
+
+    private final StringProperty title;
+    private final StringProperty category;
+    private final IntegerProperty priority;
+    private final ObjectProperty<LocalDate> dueDate;
+
+
+    public Task(String title, String category, int priority, LocalDate dueDate)
     {
         this.title = new SimpleStringProperty(title);
         this.category = new SimpleStringProperty(category);
+        this.priority = new SimpleIntegerProperty(priority);
+        this.dueDate = new SimpleObjectProperty<>(LocalDate.of(2018, 5, 6));
     }
-
 
     public String getTitle()
     {
         return title.get();
     }
 
-    public SimpleStringProperty titleProperty()
+    public StringProperty titleProperty()
     {
         return title;
     }
@@ -34,7 +47,7 @@ public class Task
         return category.get();
     }
 
-    public SimpleStringProperty categoryProperty()
+    public StringProperty categoryProperty()
     {
         return category;
     }
@@ -44,10 +57,34 @@ public class Task
         this.category.set(category);
     }
 
-    //PRIORITIES
-    final static int NONE = 0;
-    final static int LOW = 1;
-    final static int MEDIUM = 2;
-    final static int HIGH = 3;
+    public int getPriority()
+    {
+        return priority.get();
+    }
+
+    public IntegerProperty priorityProperty()
+    {
+        return priority;
+    }
+
+    public void setPriority(int priority)
+    {
+        this.priority.set(priority);
+    }
+
+    public LocalDate getDueDate()
+    {
+        return dueDate.get();
+    }
+
+    public ObjectProperty<LocalDate> dueDateProperty()
+    {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate)
+    {
+        this.dueDate.set(dueDate);
+    }
 
 }
