f14a9e2ee1190de30fa641d854ab157776552681
=Omid-PC <=nassiromid@gmail.com>
2018-12-30 12:14:56 -0800

    Added table columns for priority and due date Set cell value factories for said columns Created handleNewTask()

--- src/controllers/TaskController.java
+++ src/controllers/TaskController.java
@@ -1,17 +1,23 @@
 package controllers;
 
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
 import model.Task;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 
 public class TaskController
 {
+
+    @FXML
+    private AnchorPane anchorPane;
 
     @FXML
     private TableView<Task> tableView;
@@ -22,10 +28,12 @@ public class TaskController
     @FXML
     private TableColumn<Task, String> categoryColumn;
 
-    public TaskController()
-    {
+    @FXML
+    private TableColumn<Task, Integer> priorityColumn;
 
-    }
+    @FXML
+    private TableColumn<Task, LocalDate> dueDateColumn;
+
 
     public void loadData(ObservableList<Task> observableList)
     {
@@ -37,5 +45,29 @@ public class TaskController
     {
         titleColumn.setCellValueFactory(cellData -> cellData.getValue().titleProperty());
         categoryColumn.setCellValueFactory(cellData -> cellData.getValue().categoryProperty());
+        priorityColumn.setCellValueFactory(cellData -> cellData.getValue().priorityProperty().asObject());
+        dueDateColumn.setCellValueFactory(cellData -> cellData.getValue().dueDateProperty());
+    }
+
+    @FXML
+    private void handleNewTask()
+    {
+        //create task window
+        Dialog<ButtonType> dialogWindow = new Dialog<>();
+        dialogWindow.initOwner(anchorPane.getScene().getWindow());
+        dialogWindow.setTitle("Add New Task");
+
+        // create buttons
+        dialogWindow.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialogWindow.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+        //check which button was chosen
+        Optional<ButtonType> choice = dialogWindow.showAndWait();
+        if(choice.isPresent() && choice.get() == ButtonType.OK)
+        {
+            System.out.println("user choose - ok");
+        }
+        else
+            System.out.println("user canceled");
     }
 }
