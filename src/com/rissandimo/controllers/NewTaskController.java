69d579db4689289783a836a2676a0881d1daeafe
=Omid-PC <=nassiromid@gmail.com>
2019-01-01 15:09:54 -0800

    NewTaskController: Added newTaskIsValid(), handleOk(), setTaskDetails(), getAccessToNewTaskStage() Implemented handleCancel()

--- src/com/rissandimo/controllers/NewTaskController.java
+++ src/com/rissandimo/controllers/NewTaskController.java
@@ -1,7 +1,11 @@
 package com.rissandimo.controllers;
 
+import com.rissandimo.model.Task;
+import com.rissandimo.util.AlertUtil;
+import com.rissandimo.util.DateUtil;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class NewTaskController
 {
@@ -17,17 +21,88 @@ public class NewTaskController
     @FXML
     private TextField taskDueDateField;
 
-    @FXML
-    private void handleOk()
-    {
+    private boolean okClicked = false;
 
+    private Task task;
+
+    private Stage taskStage;
+
+    private boolean newTaskIsValid()
+    {
+        String errorMesssage = "";
+
+        String taskName = taskNameField.getText();
+        String taskCategory = taskCategoryField.getText();
+        String taskPriority = taskPriorityField.getText();
+        String taskDueDate = taskDueDateField.getText();
+
+        if(taskName == null || taskName.length() == 0)
+        errorMesssage += "Name is invalid! \n";
+
+        if(taskCategory == null || taskCategory.length() == 0)
+            errorMesssage += "Category is invalid! \n";
+
+        if(taskPriority == null || taskPriority.length() == 0)
+            errorMesssage += "Priority is invalid! \n";
+
+        if(taskDueDate == null || taskDueDate.length() == 0)
+            errorMesssage += "Due date is invalid! \n";
+
+        if (errorMesssage.length() == 0) return true;
+        else
+        {
+            AlertUtil.showError("Invalid fields", "Please make sure all fields are filled out");
+            return false;
+        }
     }
 
     @FXML
     private void handleCancel()
     {
-
+        okClicked = false;
     }
 
 
+    /**
+     * Check if user input is valid
+     * If valid -> save text input to new task
+     * mark ok clicked = true
+     * close this stage -> new person is shown
+     */
+    @FXML
+    private void handleOk()
+    {
+        if(newTaskIsValid())
+        {
+            task.setTitle(taskNameField.getText());
+            task.setCategory(taskCategoryField.getText());
+            task.setPriority(Integer.parseInt(taskPriorityField.getText()));
+            task.setDueDate(DateUtil.stringDateToLocalDate(taskDueDateField.getText()));
+
+            okClicked = true;
+
+            taskStage.close();
+        }
+
+    }
+
+    public void setTaskDetails(Task task)
+    {
+        this.task = task;
+
+        taskNameField.setText(task.getTitle());
+        taskCategoryField.setText(task.getCategory());
+        taskPriorityField.setText(Integer.toString(task.getPriority()));
+        taskDueDateField.setText(DateUtil.localDatetoStringDate(task.getDueDate()));
+    }
+
+    public void getAccessToNewTaskStage(Stage taskStageFromMainApp)
+    {
+        this.taskStage = taskStageFromMainApp;
+    }
+
+    public boolean isOkClicked()
+    {
+        return okClicked;
+    }
 }
