1d76fbaeb3b4fb2bc32420235630acc85ccc344f
=Omid-PC <=nassiromid@gmail.com>
2019-01-04 17:57:27 -0800

    Started logic for handleSearch()

--- src/com/rissandimo/controllers/TaskController.java
+++ src/com/rissandimo/controllers/TaskController.java
@@ -6,7 +6,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import com.rissandimo.model.Task;
+import javafx.scene.control.TextField;
+
 import java.time.LocalDate;
+import java.util.LinkedList;
 
 
 public class TaskController
@@ -30,6 +33,11 @@ public class TaskController
     private MainApplication mainApplication;
 
     private static final int HIGH_PRIORITY = 3;
+
+    @FXML
+    private TextField searchBar;
+
+    private LinkedList<String> taskNameEntered = new LinkedList<>();
 
 
     public void giveAccessToTaskController(MainApplication mainApplication)
@@ -55,15 +63,11 @@ public class TaskController
         Task newTask = new Task();
 
         boolean okClicked = mainApplication.showNewTaskDialog(newTask);
-        if(okClicked)
-        {
-            if(newTask.getPriority() == HIGH_PRIORITY)
-            {
+        if (okClicked) {
+            if (newTask.getPriority() == HIGH_PRIORITY) {
                 mainApplication.getTasksPriorityList().add(newTask);
                 mainApplication.getTaskObservableList().add(newTask);
-            }
-            else
-            {
+            } else {
                 mainApplication.getTaskObservableList().add(newTask);
             }
         }
@@ -72,13 +76,10 @@ public class TaskController
     @FXML
     private void handleEditTask()
     {
-        try
-        {
+        try {
             Task selectedTask = tableView.getSelectionModel().getSelectedItem();
             mainApplication.showNewTaskDialog(selectedTask);
-        }
-        catch(NullPointerException e)
-        {
+        } catch (NullPointerException e) {
             AlertUtil.showWarning("No task chosen", "Please select a task to edit");
         }
         //TODO - reload table view to show edited task
@@ -101,12 +102,9 @@ public class TaskController
     {
         Task userDeleteItem = tableView.getSelectionModel().getSelectedItem();
 
-        if(userDeleteItem == null)
-        {
-          AlertUtil.showWarning("Warning", "You must select a task to delete");
-        }
-        else
-        {
+        if (userDeleteItem == null) {
+            AlertUtil.showWarning("Warning", "You must select a task to delete");
+        } else {
             mainApplication.getTasksDeletedList().add(userDeleteItem);
             tableView.getItems().remove(userDeleteItem);
         }
@@ -118,5 +116,24 @@ public class TaskController
         tableView.setItems(mainApplication.getTasksPriorityList());
     }
 
-}
+    @FXML
+    private void handleSearch()
+    {
+        int lengthOfSearch = searchBar.getCharacters().length();
+        CharSequence charEntered = searchBar.getCharacters();
+        int currentTask = 0;
+
+        Task tempTask = mainApplication.getTaskObservableList().get(currentTask);
+
+            for(int currentChar=0; currentChar < lengthOfSearch; currentChar++)
+            {
+                if(tempTask.getTitle().charAt(currentChar) == charEntered.charAt(currentChar))
+                {
+                    System.out.println(Character.toString(charEntered.charAt(currentChar)));
+                }
+
+            }
+        }
+
+    }
 
